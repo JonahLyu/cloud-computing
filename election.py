82774e92254e2470559bafdae4cc228bd534d486
Ubuntu <ubuntu@ip-172-31-26-48.ec2.internal>
2020-12-02 08:13:10 +0000

    modify election code

--- election.py
+++ election.py
@@ -1,51 +1,52 @@
-import time, sys, kazoo, logging
+import time, kazoo, logging
 from kazoo.client import KazooClient
-from kazoo.client import KazooState
 
-MASTER_PATH = '/master'
+ELECTION_PATH = '/master'
 
 class Election:
 
-    def __init__(self, zk, election_path, self_path):
+    def __init__(self, zk, election_path, my_path):
         self.election_path = election_path
         self.zk = zk
-        self.is_leader = False
-        self.child_id = self_path.split("/")[2]
-        self.path = self_path
-        self.leader_path = None
+        self.is_master = False
+        self.my_id = my_path.split("/")[2] #/master/id_123
+        self.my_path = my_path
+        self.master_path = None
         self.kill_now =  False
-        print("My id is:  %s" % self.child_id)
+        print("My id is:  %s" % self.my_id)
 
     def kill_myself(self,signum, frame):
-        self.zk.delete(self.path)
-        if(self.is_leader) :
-            self.zk.delete(self.leader_path)
+        self.zk.delete(self.my_path)
+        if(self.is_master) :
+            self.zk.delete(self.master_path)
         self.kill_now = True	
 		 
-    def is_leading(self):
-	    return self.is_leader	
+    def is_master_server(self):
+	    return self.is_master	
 		
-
     def on_node_delete(self, event) :
         #in case of deletion start elections(perform a vote)
         if event.type == kazoo.protocol.states.EventType.DELETED:
-            print('Master died')
+            print('Master just died, new master election start...')
             self.ballot(self.zk.get_children(self.election_path))
             
 	#perform a vote..	
     def ballot(self,children):
-        next_master = min(children)  #choose the minimum one as master
-        master_path = self.election_path + "/master_current/"
+        new_master = min(children)  #choose the minimum one as master
+        master_path = self.election_path + "/master_current"
         self.zk.ensure_path(master_path) #creat master_path znode if it doesn't exist
-        self.leader_path = master_path + next_master #/master/master_current/id_123
-        if(self.child_id == next_master) :
-            self.zk.create(self.leader_path, ephemeral=True) 
-            self.is_leader = True
+        self.master_path = f'{master_path}/{new_master}' #/master/master_current/id_123
+        if(self.my_id == new_master) :
+            self.zk.create(self.master_path, ephemeral=True) 
+            self.is_master = True
+            print ("Master is: %s" % (self.master_path)) 
+            print ("I am master now") 
             return True
         else:
-            print ("Master is = %s " %(self.leader_path)) 
-            self.zk.exists(self.leader_path, self.on_node_delete) #watch the master delete event
-            self.is_leader = False
+            self.zk.exists(self.master_path, self.on_node_delete) #watch the master delete event
+            self.is_master = False
+            print ("Master is: %s" % (self.master_path)) 
+            print ("I am worker now") 
             return False
 
 
@@ -56,15 +57,12 @@ if __name__ == '__main__':
     zk = KazooClient(zkhost) 
     zk.start()
 
-    zk.ensure_path(MASTER_PATH)
-    master_path = MASTER_PATH + "/id_"
-    child = zk.create(master_path, ephemeral=True, sequence=True)
-    election = Election(zk, MASTER_PATH, child)
+    zk.ensure_path(ELECTION_PATH)
+    master_path = ELECTION_PATH + "/id_"
+    my_path = zk.create(master_path, ephemeral=True, sequence=True)
+    election = Election(zk, ELECTION_PATH, my_path)
 
-    if election.ballot(zk.get_children(MASTER_PATH)) == False :
-	    print("I'm a worker")
-    else :
-        print("I'm the leader now")
+    election.ballot(zk.get_children(ELECTION_PATH))
 
     while (election.kill_now == False) :
         time.sleep(1)
