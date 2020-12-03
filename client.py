21a71245bbad64f9c7eb6152b4f16badc9c78d6a
jonahlyu <lvjikeleo@hotmail.com>
2020-12-03 09:12:53 +0000

    implement mandelbrot worker

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 from kazoo.client import KazooClient
 import server, time, uuid
 import numpy as np
+import matplotlib.pyplot as plt
 
 ELECTION_PATH="/master"
 TASKS_PATH="/tasks"
@@ -19,7 +20,7 @@ pixels = []
 sliceNum = 3
 width = 1024
 height = 768
-zoom = 1.2
+zoom = 1
 
 # watch the workers status
 @zk.ChildrenWatch(WORKERS_PATH)
@@ -53,6 +54,7 @@ tasks = creatTask(sliceNum, width, height, zoom)
 #create result path
 newResultPath = f'{RESULTS_PATH}/{clientID}'
 newResultPath = zk.ensure_path(newResultPath)
+print("create: %s" % newResultPath)
 
 for i in range(len(tasks)):
     # 1. create /params/task_id to store the parameters of task
@@ -62,15 +64,36 @@ for i in range(len(tasks)):
     taskID = newParamPath.split('/')[2]
     newTaskPath = f'{TASKS_PATH}/{taskID}'
     newTaskPath = zk.create(newTaskPath, clientID.encode("utf-8"), ephemeral=True)
-    print(newParamPath)
-    print(newTaskPath)
+    print("create: %s" % newParamPath)
+    print("create: %s" % newTaskPath)
     
+
+start = time.time()
 
 # watch the result status
 @zk.ChildrenWatch(newResultPath)
 def watch_result(results):
-    print("results: %s" % (results))
+    global pixels, count, width
+    count = len(results)
+    print("task progress: %d/%d" % (count, sliceNum))
 
-print(newResultPath)
+
+while count < sliceNum:
+    pass
+end = time.time()
+print("Total time: %.2f seconds" % (end - start))
+
+results = zk.get_children(newResultPath)
+for taskID in results: 
+    data, _ = zk.get(f"{newResultPath}/{taskID}")
+    paramsData, _ = zk.get(f"{PARAMS_PATH}/{taskID}")
+    paramsData = paramsData.decode("utf-8")
+    params = paramsData.split(':')
+    startRow,  endRow= int(params[3]), int(params[4])
+    pixelSlice = np.frombuffer(data, dtype=np.uint16).reshape(endRow-startRow,width)
+    pixels[startRow:endRow] = pixelSlice
+plt.axis('off')
+plt.imshow(pixels)
+plt.savefig("result.png")
 while True:
     time.sleep(1)
--- worker.py
+++ worker.py
@@ -3,12 +3,35 @@ from kazoo.protocol.states import EventType
 from kazoo.client import KazooClient
 from election import Election
 import server
+import numpy as np
 ELECTION_PATH="/master"
 TASKS_PATH="/tasks"
 PARAMS_PATH="/params"
 WORKERS_PATH="/workers"
 STATUS_PATH="/status"
 RESULTS_PATH="/results"
+
+
+def mandelbrot_set(width, height, startRow, endRow, zoom=1, x_off=0, y_off=0, niter=256):
+    w,h = width, height
+    pixels = np.arange(w*(endRow - startRow), dtype=np.uint16).reshape(endRow - startRow, w)
+
+    for x in range(w): 
+        zx = 1.5*(x + x_off - 3*w/4)/(0.5*zoom*w)
+        for y in range(startRow, endRow):
+            
+            zy = 1.0*(y + y_off - h/2)/(0.5*zoom*h)
+            
+            z = complex(zx, zy)
+            c = complex(0, 0)
+            
+            for i in range(niter):
+                if abs(c) > 4: break
+                c = c**2 + z
+
+            color = (i << 21) + (i << 10)  + i * 8
+            pixels[y - startRow][x] = color
+    return pixels
 
 class Worker:
 
@@ -35,17 +58,20 @@ class Worker:
             paramPath = f'{PARAMS_PATH}/{taskID}'
             taskPath = f'{TASKS_PATH}/{taskID}'
             clientID, _ = self.zk.get(taskPath)
-            clientID = clientID.decode("utf-8").split('#')[2]
+            clientID = clientID.decode("utf-8").split('#')[1]
+            print(clientID)
             if self.zk.exists(paramPath) :
                 data, _ = self.zk.get(paramPath)
                 #6. execute task with data
-                result = data.decode("utf-8")
-                time.sleep(10)
+                params = data.decode("utf-8").split(':')
+                width, height, zoom = int(params[0]),int(params[1]),float(params[2])
+                startRow, endRow = int(params[3]), int(params[4])
+                pixels = mandelbrot_set(width, height, startRow, endRow, zoom)
                 taskPath = f'{TASKS_PATH}/{taskID}'
                 resultPath = f'{RESULTS_PATH}/{clientID}/{taskID}'
                 # write result back to task
                 if self.zk.exists(taskPath):
-                    self.zk.create(resultPath, result.encode("utf-8"))
+                    self.zk.create(resultPath, pixels.tobytes())
                     self.zk.set(taskPath, b"complete")
                     #free this worker
                     self.zk.set(self.statusPath, b"non")
@@ -54,6 +80,9 @@ class Worker:
                     #free this worker
                     self.zk.set(self.statusPath, b"non")
                     print("Task %s not found, maybe the connection  was lost" %(resultPath))
+
+
+
 
 if __name__ == '__main__':
     zk = server.init()    
